3f3c5599bbe2b0180500369045ae15d2d664dcf4
SatyamDevX <satyam.development@gmail.com>
2025-04-09 00:20:59 +0530

    printing the file_info on uploading a file/ photo

--- photovault/app.py
+++ photovault/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 
 app = Flask(__name__)
@@ -10,7 +10,31 @@ def home():
 
 @app.route('/', methods=['POST'])
 def upload_image():
-   return "post route is working, now write the logic to upload it into /static/uploads"
+
+   file = request.files.get('file')
+
+   if file:
+      content = file.read()
+      size_bytes = len(content)
+
+      file_info = {
+         'filename': file.filename,
+         'content_type': file.content_type,
+         'size_kb': round(size_bytes / 1024, 2),
+         'size_mb': round(size_bytes / (1024 * 1024), 2)
+      }
+
+      file.stream.seek(0)  # Reset stream if needed later
+
+      return file_info
+   else:
+      return {'error': 'No file uploaded'}
+
+
+
+
+
+   
 
 if __name__ == "__main__":
    app.run()
